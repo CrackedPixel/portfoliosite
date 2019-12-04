c0d48e0f185e3134aea898c5099e07b3a358c97e
CrackedPixel <acarbajal721@live.com>
2019-12-04 14:13:30 -0700

    Removed gradients

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -6,7 +6,7 @@ export const WelcomePage = () => {
     <div className="page welcome" id="welcome">
       <h1><span className="col1">A</span><span className="col2">nthony</span>&nbsp;<span className="col1">Carb</span><span className="col2">ajal</span></h1>
       <p>Just another <a href="https://lambdaschool.com/" target="_blank" rel="noopener noreferrer">Lambda School</a> student.<br />Feel free to browse, there's no fee until you hire me.</p>
-      <MainButton href="#aboutme" posticon="far fa-chevron-right" text="about me"/>
+      <MainButton href="#aboutme" posticon="fa fa-chevron-right" text="about me"/>
     </div>
   )
 }
