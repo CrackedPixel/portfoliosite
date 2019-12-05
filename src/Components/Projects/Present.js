c10bba8c3d7dafe15dfc83bb46e04c484f950424
CrackedPixel <acarbajal721@live.com>
2019-12-04 18:18:19 -0700

    Working on fixing FA bugs

--- src/Components/Projects/Present.js
+++ src/Components/Projects/Present.js
@@ -5,11 +5,11 @@ export const Present = (props) => {
   return (
     <div className="project__present__container">
       <div className="present__left">
-        <button className="project_btn" disabled={props.curImage === 0} onClick={() => props.setNewCurImage(-1)}><i className="fas fa-arrow-circle-left"></i></button>
+        <button className="project_btn" disabled={props.curImage === 0} onClick={() => props.setNewCurImage(-1)}><i className="fa fa-arrow-circle-left"></i></button>
         <div className="present__preview__holder">
           <img className="present__preview__img" src={props.cp.pics[props.curImage]} alt={props.cp.title + " image " + props.curImage}/>
         </div>
-        <button className="project_btn" disabled={props.curImage === props.cp.pics.length-1} onClick={() => props.setNewCurImage(1)}><i className="fas fa-arrow-circle-right"></i></button>
+        <button className="project_btn" disabled={props.curImage === props.cp.pics.length-1} onClick={() => props.setNewCurImage(1)}><i className="fa fa-arrow-circle-right"></i></button>
       </div>
       <div className="present__right">
         <h4>{props.cp.title}</h4>
