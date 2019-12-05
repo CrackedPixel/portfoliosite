cccfd847094d3fbc627e51dfdc30343327b00bd9
CrackedPixel <acarbajal721@live.com>
2019-12-04 20:55:31 -0700

    Fixed carousel fade manual click selctor

--- src/Components/Projects/Selector.js
+++ src/Components/Projects/Selector.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Selector = (props) => {
   return (
     <div className="project__selector__obj" onClick={props.clicky} value={props.projid}>
-      <img className={props.curSelectedProj === props.projid ? "project__selector__img" : "project__selector__img dimmed"} src={props.src} alt={props.alt} value={props.projid}/>
+      <img className={parseInt(props.curSelectedProj) === parseInt(props.projid) ? "project__selector__img" : "project__selector__img dimmed"} src={props.src} alt={props.alt} value={props.projid}/>
     </div>
   )
 }
