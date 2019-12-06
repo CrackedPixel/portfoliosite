e75eac7650834d6a9b5a24c387e4aa1f1f55a907
CrackedPixel <acarbajal721@live.com>
2019-12-05 17:20:43 -0700

    Added hovers to nav and social

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -9,7 +9,7 @@ export const AboutMe = () => {
       <p>I grew up on a mountain, in a small town near Portland, Oregon. I have a burning passion for technology and I'm always excited about new developments and ideas. I thrive in team-based environments with lots of collaboration, but am fully capable of tackling solo jobs as well. I am motivated by my passion for creation, learning, and bettering myself.</p>
       <p>I'm currently working as the lead programmer at a game studio, but I am always looking for new and exciting projects to work on.</p>
       <p>Please feel free to review my resume or contact me for more information</p>
-      <MainButton href="https://google.com" target="_blank" preicon="fa fa-download" text="resume"/>
+      <MainButton href="RESUME_CURRENT.pdf" target="_blank" preicon="fa fa-download" text="resume"/>
     </div>
   )
 }
